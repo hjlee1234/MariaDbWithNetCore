616c0e9a4eea6cb3526c5b2fd7b9c49436b62494
Masoud Daneshpour <masoud.vb@gmail.com>
2019-05-19 04:41:30 -0700

    FEATURE : Add GetGroup Handler

---
+++ MariaDbWithNetCore.Api/Queries/Group/GetGroupByIdHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace MariaDbWithNetCore.Api.Queries.Group
+{
+    public class GetGroupByIdHandler : IRequestHandler<GetGroupByIdRequest, Entity.Group>
+    {
+        private readonly MariaDbContext _dbContext;
+        public GetGroupByIdHandler(MariaDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Entity.Group> Handle(GetGroupByIdRequest request, CancellationToken cancellationToken)
+        {
+            return await _dbContext.Groups.FirstOrDefaultAsync(c => c.GroupId == request.Id, cancellationToken);
+        }
+    }
+}
---
+++ MariaDbWithNetCore.Api/Queries/Group/GetGroupByIdRequest.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace MariaDbWithNetCore.Api.Queries.Group
+{
+    public class GetGroupByIdRequest : IRequest<Entity.Group>
+    {
+        public int Id { get; set; }
+
+        public GetGroupByIdRequest(int id)
+        {
+            Id = id;
+        }
+    }
+}
