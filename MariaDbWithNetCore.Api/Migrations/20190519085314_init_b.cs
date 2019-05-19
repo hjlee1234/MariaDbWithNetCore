fa52d265bbea73762d4cdd47e312c9e19fecdc93
Masoud Daneshpour <M.daneshpour@Kheilisabz.com>
2019-05-19 02:01:38 -0700

    FEATURE: Add table initialization configurations

---
+++ MariaDbWithNetCore.Api/Migrations/20190519085314_init_b.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MariaDbWithNetCore.Api.Migrations
+{
+    public partial class init_b : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Group",
+                columns: table => new
+                {
+                    GroupId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    LatinName = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    ImageUrl = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Group", x => x.GroupId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Store",
+                columns: table => new
+                {
+                    StoreId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Code = table.Column<string>(nullable: true),
+                    Location = table.Column<string>(nullable: true),
+                    Grade = table.Column<string>(nullable: true),
+                    StartTime = table.Column<short>(nullable: false),
+                    EndTime = table.Column<short>(nullable: false),
+                    Telephone = table.Column<string>(nullable: true),
+                    Mobile = table.Column<string>(nullable: true),
+                    Address = table.Column<string>(nullable: true),
+                    Responsible = table.Column<string>(nullable: true),
+                    IBAN = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Store", x => x.StoreId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    LatinName = table.Column<string>(nullable: true),
+                    ProductCode = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    Summery = table.Column<string>(nullable: true),
+                    Type = table.Column<string>(nullable: true),
+                    Size = table.Column<string>(nullable: true),
+                    UnitSize = table.Column<short>(nullable: false),
+                    GroupId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.ProductId);
+                    table.ForeignKey(
+                        name: "FK_Product_Group_GroupId",
+                        column: x => x.GroupId,
+                        principalTable: "Group",
+                        principalColumn: "GroupId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StoreShare",
+                columns: table => new
+                {
+                    StoreId = table.Column<int>(nullable: false),
+                    GroupId = table.Column<int>(nullable: false),
+                    SharePercent = table.Column<short>(nullable: false, defaultValue: (short)0)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StoreShare", x => new { x.StoreId, x.GroupId });
+                    table.UniqueConstraint("AK_StoreShare_GroupId_StoreId", x => new { x.GroupId, x.StoreId });
+                    table.ForeignKey(
+                        name: "FK_StoreShare_Group_GroupId",
+                        column: x => x.GroupId,
+                        principalTable: "Group",
+                        principalColumn: "GroupId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_StoreShare_Store_StoreId",
+                        column: x => x.StoreId,
+                        principalTable: "Store",
+                        principalColumn: "StoreId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductImage",
+                columns: table => new
+                {
+                    ProductImageId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ProductId = table.Column<int>(nullable: false),
+                    Url = table.Column<string>(nullable: true),
+                    Order = table.Column<short>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductImage", x => x.ProductImageId);
+                    table.ForeignKey(
+                        name: "FK_ProductImage_Product_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Product",
+                        principalColumn: "ProductId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductInStore",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(nullable: false),
+                    StoreId = table.Column<int>(nullable: false),
+                    HasShortage = table.Column<bool>(nullable: false, defaultValue: false),
+                    ShortageDateTime = table.Column<DateTime>(nullable: false, defaultValue: new DateTime(2019, 5, 19, 1, 53, 13, 915, DateTimeKind.Local).AddTicks(7586))
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductInStore", x => new { x.ProductId, x.StoreId });
+                    table.ForeignKey(
+                        name: "FK_ProductInStore_Product_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Product",
+                        principalColumn: "ProductId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProductInStore_Store_StoreId",
+                        column: x => x.StoreId,
+                        principalTable: "Store",
+                        principalColumn: "StoreId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Product_GroupId",
+                table: "Product",
+                column: "GroupId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductImage_ProductId",
+                table: "ProductImage",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductInStore_StoreId",
+                table: "ProductInStore",
+                column: "StoreId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductImage");
+
+            migrationBuilder.DropTable(
+                name: "ProductInStore");
+
+            migrationBuilder.DropTable(
+                name: "StoreShare");
+
+            migrationBuilder.DropTable(
+                name: "Product");
+
+            migrationBuilder.DropTable(
+                name: "Store");
+
+            migrationBuilder.DropTable(
+                name: "Group");
+        }
+    }
+}
