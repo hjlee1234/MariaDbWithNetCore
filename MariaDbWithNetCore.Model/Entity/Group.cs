8ccdf3f902b9d97e049199eeaad987a81d3d81b3
Masoud Daneshpour <M.daneshpour@Kheilisabz.com>
2019-05-19 01:59:43 -0700

    FEATURE: Add group table

---
+++ MariaDbWithNetCore.Model/Entity/Group.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace MariaDbWithNetCore.Model.Entity
+{
+    [Table(nameof(Group))]
+    public class Group
+    {
+        [Key]
+        public int GroupId { get; set; }
+        public string Name { get; set; }
+        public string LatinName { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+    }
+}
