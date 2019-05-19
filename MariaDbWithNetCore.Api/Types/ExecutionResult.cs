4d7aa2f0d3594afedaa347e6c38750c60131a247
Masoud Daneshpour <masoud.vb@gmail.com>
2019-05-19 04:41:47 -0700

    FEATURE : Add execution handler

---
+++ MariaDbWithNetCore.Api/Types/ExecutionResult.cs
@@ -0,0 +1,9 @@
+﻿namespace MariaDbWithNetCore.Api.Types
+{
+    public class ExecutionResult<T>
+    {
+        public bool State { get; set; }
+        public string Message { get; set; }
+        public T Result { get; set; }
+    }
+}
