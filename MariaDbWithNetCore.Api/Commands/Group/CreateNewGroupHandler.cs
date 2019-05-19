2c27b682dd5424951835678d6f2d7d4169c860f5
Masoud Daneshpour <masoud.vb@gmail.com>
2019-05-19 04:39:52 -0700

    FEATURE : Add Create group handler

---
+++ MariaDbWithNetCore.Api/Commands/Group/CreateNewGroupHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using MariaDbWithNetCore.Api.Types;
+
+using MediatR;
+
+namespace MariaDbWithNetCore.Api.Commands.Group
+{
+    public class CreateNewGroupHandler : IRequestHandler<CreateNewGroupRequest, ExecutionResult<int>>
+    {
+        private readonly MariaDbContext _dbContext;
+        public CreateNewGroupHandler(MariaDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<ExecutionResult<int>> Handle(CreateNewGroupRequest request, CancellationToken cancellationToken)
+        {
+            var result = new ExecutionResult<int>();
+
+            try
+            {
+                await _dbContext.Groups.AddAsync(request.Model,cancellationToken);
+                await _dbContext.SaveChangesAsync(cancellationToken);
+                result.State = true;
+            }
+            catch (Exception e)
+            {
+                result.State = false;
+                result.Message = e.Message;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ MariaDbWithNetCore.Api/Commands/Group/CreateNewGroupRequest.cs
@@ -0,0 +1,16 @@
+﻿
+using MariaDbWithNetCore.Api.Types;
+
+using MediatR;
+
+namespace MariaDbWithNetCore.Api.Commands.Group
+{
+    public class CreateNewGroupRequest : IRequest<ExecutionResult<int>>
+    {
+        public Entity.Group Model { get; set; }
+        public CreateNewGroupRequest(Entity.Group model)
+        {
+            Model = model;
+        }
+    }
+}
