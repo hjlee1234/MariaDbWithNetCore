ac7e21bb57982974ce0eeba91a3c64df14a55fa8
Masoud Daneshpour <M.daneshpour@Kheilisabz.com>
2019-05-19 02:01:21 -0700

    FEATURE: Add API project

---
+++ MariaDbWithNetCore.Api/Startup.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MariaDbWithNetCore.Api
+{
+    public class Startup
+    {
+        private IConfiguration _configuration;
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<MariaDbContext>(options =>
+                options.UseMySql(_configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddMvc();
+        }
+
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
